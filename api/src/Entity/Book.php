580c118762bc8f011094868b798d344f51c43cc0
daproclaima <sebastien@devedanos.com>
2021-10-30 19:17:53 +0200

    add annotations in /Entity/Book.php

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -5,30 +5,65 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
 
-/** A book. */
+/**
+ * A book.
+ *
+ * @ORM\Entity
+ */
 #[ApiResource]
 class Book
 {
-    /** The id of this book. */
+    /**
+     * The id of this book.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
     private ?int $id = null;
 
-    /** The ISBN of this book (or null if doesn't have one). */
+    /**
+     * The ISBN of this book (or null if doesn't have one).
+     *
+     * @ORM\Column(nullable=true)
+     */
     public ?string $isbn = null;
 
-    /** The title of this book. */
+    /**
+     * The title of this book.
+     *
+     * @ORM\Column
+     */
     public string $title = '';
 
-    /** The description of this book. */
+    /**
+     * The description of this book.
+     *
+     * @ORM\Column(type="text")
+     */
     public string $description = '';
 
-    /** The author of this book. */
+    /**
+     * The author of this book.
+     *
+     * @ORM\Column
+     */
     public string $author = '';
 
-    /** The publication date of this book. */
+    /**
+     * The publication date of this book.
+     *
+     * @ORM\Column(type="datetime_immutable")
+     */
     public ?\DateTimeInterface $publicationDate = null;
 
-    /** @var Review[] Available reviews for this book. */
+    /**
+     * @var Review[] Available reviews for this book.
+     *
+     * @ORM\OneToMany(targetEntity="Review", mappedBy="book", cascade={"persist", "remove"})
+     */
     public iterable $reviews;
 
     public function __construct()
